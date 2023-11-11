138ae32555b4abe238a231dbb629598647eb7dc9
Dillon Luther <dillon.luther@gmail.com>
2023-11-10 22:29:43 -0700

    text adjustments

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,27 +1,27 @@
 const HomePage: React.FC = () => {
   return (
     <div className="px-8 mx-auto my-8 w-full sm:w-160">
-      <h1 className="my-8 text-2xl font-bold tracking-wide">Dillon Luther</h1>
-      <p className="my-4">
+      <h1 className="my-8 text-xl font-bold tracking-wide">Dillon Luther</h1>
+      <p className="my-4 text-base">
         Hi, I'm Dillon! I'm a mechanical engineer turned software engineer who lives in New York City.
       </p>
-      <p className="my-4">
+      <p className="my-4 text-base">
         <strong>Currently</strong>
         , I'm a software engineer at CoinList who primarily builds the Token Sales product.
       </p>
-      <p className="my-4">
+      <p className="my-4 text-base">
         <strong>Previously</strong>
         , I was at professional West Coast Swing dancer, worked at Cisco as a mechanical product engineer, and graduated from Arizona State University.
       </p>
-      <p className="my-4">
+      <p className="my-4 text-base">
         <strong>For fun</strong>
         , I enjoy dancing, playing most sports (especially pickleball and volleyball), being around people, and honing new skills. I'm currently training to dunk a basketball.
       </p>
-      <p className="my-4">
+      <p className="my-4 text-base">
         <strong>I want to</strong>
         &nbsp;learn another language, build my own software application, live in another country, and optimize my fitness.
       </p>
-      <p className="my-4">
+      <p className="my-4 text-base">
         <strong>Connect</strong>
          &nbsp;with me on&nbsp;
         <a className="text-blue-800 hover:underline" href="https://twitter.com/dtluther">Twitter</a>
