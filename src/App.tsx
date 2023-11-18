6225b1fa97efa91dd68c8af23d18e88732462257
Dillon Luther <dillon.luther@gmail.com>
2023-11-17 22:21:48 -0600

    playing with new home

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/Home";
+import NewHomePage from "./pages/NewHome";
 import TimelinePage from "./pages/Timeline";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/timeline" element={<TimelinePage />} />
+        <Route path="/newhome" element={<NewHomePage />} />
       </Routes>
     </Router>
   );
