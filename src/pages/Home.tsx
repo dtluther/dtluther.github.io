88eefe66e47819d6e13011704c6f57a6cc684fb4
Dillon Luther <dillon.luther@gmail.com>
2024-12-26 01:04:16 -0700

    Add React components and styles

---
+++ src/pages/Home.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Home.css'
+
+const Home: React.FC = () => {
+  return (
+    <div className="home">
+      <header className="masthead">
+        <div className="header-content">
+          <div className="header-content-inner">
+            <h1>Dillon Luther</h1>
+            <h5>Software Developer</h5>
+          </div>
+        </div>
+      </header>
+    </div>
+  )
+}
+
+export default Home
