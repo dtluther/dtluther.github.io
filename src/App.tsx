18ec0a98bbd18135db83898bc20c93e5c2214143
Dillon Luther <dillon.luther@gmail.com>
2024-12-26 19:50:09 -0700

    Setup home page with timeline

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import "./App.css";
+import HomePage from "./pages/Home";
 
 function App() {
-  return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
-  );
+  return <HomePage />;
 }
 
 export default App;
